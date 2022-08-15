67834e52352bbb0db61cd521e52ad747fbc169c5
Miriam Fainshtein <104686417+MiriamFain@users.noreply.github.com>
2022-08-15 19:34:28 +0300

    fixed bugs

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,18 @@
-import Player from "@vimeo/player";
-import throttle from "lodash.throttle";
+import Vimeo from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
-const iframe = document.querySelector("iframe");
-const player = new Player(iframe);
-const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
 
-const onPlay = function (data) {
-  localStorage.setItem(LOCALSTORAGE_KEY, `${data.seconds}`);
-  console.log(localStorage);
-};
+player.on('loaded', () => {
+  const currentTime = localStorage.getItem(LOCAL_STORAGE_KEY) || 0;
+  player.setCurrentTime(currentTime);
+});
 
-player.on("timeupdate", throttle(onPlay, 1000));
-
-const videoPlayerTime = localStorage.getItem(LOCALSTORAGE_KEY);
-player.setCurrentTime(videoPlayerTime);
+player.on(
+  'timeupdate',
+  _throttle(({ seconds }) => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, seconds);
+  }, 1000)
+);
